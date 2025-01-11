c1c20721a3e7640e5140f3cda9deaaffae3f2718
Andreea <andreea-madalina.busuioc@s.unibuc.ro>
2025-01-11 16:13:20 +0200

    Acest program ilustreaza jocul Flappy bird, in care p reprezinta pauza sau se continua jocul dupa apasarea acestei taste

--- game.cpp
+++ game.cpp
@@ -29,7 +29,7 @@ public:
     }
 
     // Funcție pentru zbor
-    void fly() {
+    /*void fly() {
         if (height <= 0) {
             throw ImpossibleOperationException();
         }
@@ -38,7 +38,7 @@ public:
     }
 
     // Funcție pentru cădere
-    /*void fall() {
+    void fall() {
         if (height <= 0) {
             throw ImpossibleOperationException();
         }
